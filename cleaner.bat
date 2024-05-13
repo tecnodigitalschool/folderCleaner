22534574562f956c85a71ccce5db9614cd9d7ae6
TecnoDigital School <contact@tecnodigitalschool.com>
2024-05-13 23:48:04 +0200

    Add bat file

---
+++ cleaner.bat
@@ -0,0 +1,33 @@
+:: This is a comment
+:: Delete files with the following extensions
+
+@echo off
+del *.msg
+del *.dmp
+del *.pyc
+del *.sim
+del *.com
+del *.ipm
+del *.log
+del *.res
+del *.abq
+del *.pac
+del *.sel
+del *.mdl
+del *.stt
+del *.rec
+del *.simlog
+del *.lck
+del *.cid
+del *.*~
+del *.prt
+
+:: only try to delete if finds 
+:: if exist *.prt del *.prt
+
+set /P delfiles=Delete jnl, rpy, dat and sta files (y/n)? 
+if %delfiles%==y del *.jnl
+if %delfiles%==y del *.dat
+if %delfiles%==y del *.sta
+if %delfiles%==y del *.rpy
+if %delfiles%==y del *.rpy.*
